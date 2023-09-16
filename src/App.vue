d8b1ef8443f5bdb0e7ece9e93cb4c7e5ff62a663
Techzjc <techzjc@vt.edu>
2023-09-16 16:15:46 -0400

    style: 💄 Reformatted the vue-js code in this proj

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <script setup>
- import {ref, onMounted} from 'vue'
- import { useCookies } from 'vue3-cookies'
- import ChatroomList from "@/components/ChatroomList.vue";
- import LoginPage from "@/components/LoginPage.vue";
- import axios from "axios";
+import {ref, onMounted} from 'vue'
+import {useCookies} from 'vue3-cookies'
+import ChatroomList from "@/components/ChatroomList.vue";
+import LoginPage from "@/components/LoginPage.vue";
+import axios from "axios";
 
- let $cookies = useCookies().cookies
- let user_id = ref($cookies.isKey("user_id"))
+let $cookies = useCookies().cookies
+let user_id = ref($cookies.isKey("user_id"))
 
- if (user_id.value) {
-     axios.get("http://127.0.0.1:5678/api/v1/user/").then(function (response) {
-         if (response.data.status !== 0) {
-             user_id.value = false
-             console.log("Cannot find the user!")
-             $cookies.remove('user_id')
-         }
-     }
-     )
- }
+if (user_id.value) {
+  axios.get("http://127.0.0.1:5678/api/v1/user/").then(function (response) {
+        if (response.data.status !== 0) {
+          user_id.value = false
+          console.log("Cannot find the user!")
+          $cookies.remove('user_id')
+        }
+      }
+  )
+}
 
- const userTheme = ref("light")
+const userTheme = ref("light")
 
 </script>
 
 <template>
-    <lay-config-provider :theme="userTheme">
-	<LoginPage @theme-toggled="userTheme = userTheme === 'light' ? 'dark' : 'light'" v-if="!user_id" :theme="userTheme" />
-	<ChatroomList @theme-toggled="userTheme = userTheme === 'light' ? 'dark' : 'light'" v-else :theme="userTheme" />
-    </lay-config-provider>
+  <lay-config-provider :theme="userTheme">
+    <LoginPage @theme-toggled="userTheme = userTheme === 'light' ? 'dark' : 'light'" v-if="!user_id"
+               :theme="userTheme"/>
+    <ChatroomList @theme-toggled="userTheme = userTheme === 'light' ? 'dark' : 'light'" v-else :theme="userTheme"/>
+  </lay-config-provider>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'w3-css/w3.css';
 import Layui from '@layui/layui-vue'
 import '@layui/layui-vue/lib/index.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
 createApp(App).use(Layui).mount('#app')
