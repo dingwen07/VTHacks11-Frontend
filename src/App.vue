886207f8f192bdcd0c8b603b51618b1c257005c4
Techzjc <techzjc@vt.edu>
2023-09-16 10:34:05 -0400

    feat: 🎸 Add the user id detection process for the application

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script setup>
+import {ref} from 'vue'
+import { useCookies } from 'vue3-cookies'
+import ChatroomList from "@/components/ChatroomList.vue";
+import LoginPage from "@/components/LoginPage.vue";
+import axios from "axios";
+
+let $cookies = useCookies().cookies
+let user_id = ref($cookies.isKey("user_id"))
+
+if (user_id.value) {
+  axios.get("http://127.0.0.1:5678/api/v1/user/").then(function (response) {
+        if (response.data.status !== 0) {
+          user_id.value = false
+          console.log("Cannot find the user!")
+          $cookies.remove('user_id')
+        }
+      }
+  )
+}
+
 
 </script>
 
 <template>
+    <LoginPage v-if="!user_id"/>
+    <ChatroomList v-else />
 
 </template>
 
 <style scoped>
-
 </style>
