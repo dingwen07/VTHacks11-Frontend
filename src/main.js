6aa099ded2fd65216019833986994b1bf31f888a
Techzjc <techzjc@vt.edu>
2023-09-16 03:49:03 -0400

    feat: 🎸 Add ui framework for this proj

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import './assets/main.css'
+import 'w3-css/w3.css';
+
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(Layui).mount('#app')
