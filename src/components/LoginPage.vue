82fa97bc23b9d415af738921d0468688063d70d6
Techzjc <techzjc@vt.edu>
2023-09-16 11:32:17 -0400

    style: 💄 Change the UI for LoginPage

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,42 +1,58 @@
 <script setup>
-import { useCookies } from 'vue3-cookies'
+import {useCookies} from 'vue3-cookies'
+
 let $cookies = useCookies().cookies
 </script>
 
 <template>
-    <lay-card shadow="hover" class="center-element">
-      <lay-row>
-        <lay-col>
-          <h2>Welcome!</h2>
-        </lay-col>
-        <lay-col>
-          <h3>Please specify your name here:</h3>
-        </lay-col>
-        <br/>
-        <lay-col>
-          <lay-input placeholder="Your name" v-model="name" :allow-clear="true"/>
-        </lay-col>
-        <br/>
-        <lay-col>
-          <lay-button type="primary" @click="join" md="8" mdOffset="16">Join</lay-button>
-        </lay-col>
-      </lay-row>
-    </lay-card>
+  <lay-card shadow="always" class="center-element layui-anim layui-anim-scale">
+    <h2>Welcome to <span>Chatroom</span>!</h2>
+    <br />
+    <h3>Please specify your name here:</h3>
+    <br/>
+    <lay-input placeholder="Your name" v-model="name" :allow-clear="true"/>
+    <br/>
+    <br/>
+    <br/>
+    <lay-button type="primary" @click="join">Join</lay-button>
+  </lay-card>
 </template>
 
 <style scoped>
-.center-element {
-  /* Center the content */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  height: 50vh;
-  width: 50vw;
-  /* Offset the content */
-  margin-left: 25vw;
-  margin-top: 25vh;
-  /* Make the border */
-  border: 1px solid #e6e6e6;
-  background: white;
+@media screen and (min-width: 620px) {
+  .center-element {
+    /* Center the content */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 35vh;
+    width: var(--width);
+    /* Offset the content */
+    /* Convert the vh to vw*/
+    --width: 50vh;
+    margin-left: calc((100vw - var(--width)) * 0.5);
+    margin-top: 25vh;
+    /* Make the border */
+    border: 1px solid #e6e6e6;
+    background: white;
+  }
 }
+
+@media screen and (max-width: 620px) {
+  .center-element {
+    /* Center the content */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    /* Make the border */
+    border: 1px solid #e6e6e6;
+    background: white;
+  }
+}
+
+span {
+  font-weight: bolder;
+}
+
 </style>
